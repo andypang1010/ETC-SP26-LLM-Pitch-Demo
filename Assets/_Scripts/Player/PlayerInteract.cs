933fd2eb0b636fc8b7f9ae937bc99235661dfac1
Andy Pang <andypang1010@gmail.com>
2025-11-04 17:18:31 -0500

    improve interact accuracy

--- Assets/_Scripts/Player/PlayerInteract.cs
+++ Assets/_Scripts/Player/PlayerInteract.cs
@@ -3,24 +3,34 @@ using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
+    public float interactRange = 2f;
     public bool hasNPCInRange = false;
     void Update()
     {
-        if (InputController.Instance.GetInteractDown())
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 3f))
-            {
-                if (hit.transform.root.TryGetComponent(out LLMCharacter _))
-                {
-                    hasNPCInRange = true;
-                }
 
-                else
-                {
-                    hasNPCInRange = false;
-                }
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, interactRange))
+        {
+            if (hit.transform.root.TryGetComponent(out LLMCharacter _))
+            {
+                hasNPCInRange = true;
+            }
+
+            else
+            {
+                hasNPCInRange = false;
             }
         }
+        
+        else
+        {
+            hasNPCInRange = false;
+        }
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = hasNPCInRange ? Color.green : Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * interactRange);
     }
 }
